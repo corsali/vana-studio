a7b9952645e1b9cbb237da61c1e5919b3f6dfc21
Tim Nunamaker <tim@vana.com>
2023-01-06 10:39:33 -0600

    Add rough markup

--- components/Generator.js
+++ components/Generator.js
@@ -20,7 +20,7 @@ const Generator = () => {
             ok: true,
             status: 200,
             json: () => {
-              return { imageUrl: 'https://storage.googleapis.com/data-collective-images/tim%40vana.com/exhibits/tarot/tarot_6_1671240502.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=vana-app-user%40corsali-production.iam.gserviceaccount.com%2F20230105%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230105T232710Z&X-Goog-Expires=601&X-Goog-SignedHeaders=host&X-Goog-Signature=d7b9d3770b5dc94fe77cebd27ee30f2f1a3896f190184ffd8c88f17cd646e53fe218e9ca632267de6eb0c80b11d97fa89b6b89f739f4e5a774e3cd9300bef37ee5a7bb9a0b7cdb0cc3f92c90804d2f44808490ceb18ef119010bee0380d8f8e5f572e6ebcce404332c829e047b7e18c30b136bf30522c24913725b093be911bfc6774e7c5b8f0cf1c31883149cbb85cc11b511527139a521b426d352e7a124fc1a9470fcc03934284c8d22f54496619e43be7f55df31f2d975aab9a6f84fc92b4bee39e9f34688fbedbfdd1c91b56392c7f69e70ec61171906f69dd708c32107453a42cec183b17b5c9fe22a03d694cb78386dfbf6e7c9ab62eec48c2ccab338' };
+              return { imageUrl: 'https://portrait.vana.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F87172%2F1670918662-2053232767_go_soo_jung_closeup_portrait-beautiful_flat_illustrated_water_smoke_portrait-realistic_abstract_et.png%3Fauto%3Dformat%26q%3D40%26w%3D1200&w=640&q=75' };
             }
           });
         }, 1000); // simulate 1 second of latency
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,47 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Generator from '../components/Generator'
 
+import { useState } from 'react';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [user, setUser] = useState();
+  const [loginState, setLoginState] = useState();
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const logIn = async (code) => {
+    try {
+      const response = await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            ok: true,
+            status: 200,
+            json: () => {
+              return {
+                images: ['https://portrait.vana.com/_next/image?url=https%3A%2F%2Fwww.datocms-assets.com%2F87172%2F1670918662-2053232767_go_soo_jung_closeup_portrait-beautiful_flat_illustrated_water_smoke_portrait-realistic_abstract_et.png%3Fauto%3Dformat%26q%3D40%26w%3D1200&w=640&q=75']
+              };
+            },
+          });
+        }, 1000);
+      });
+
+      if (response.status !== 200) {
+        throw new Error("An error occurred while logging in");
+      }
+      const data = await response.json();
+      if (response.ok) {
+        setUser({ images: data.images });
+      } else {
+        setErrorMessage(data.message);
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -24,7 +59,88 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Generator/> 
+          {!loginState && (
+            <div>
+              <h1>Welcome to Vana Boilerplate</h1>
+              <button
+                onClick={() =>
+                  window
+                    .open("https://portrait.vana.com/create", "_blank")
+                    .focus()
+                }
+              >
+                Create your Portrait
+              </button>
+              <p>New to Vana?</p>
+              <p>Already have an account?</p>
+              <button onClick={() => setLoginState("promptEmail")}>
+                Login
+              </button>
+            </div>
+          )}
+
+          {loginState === "promptEmail" && (
+            <div>
+              <h1>Login with Vana</h1>
+              <form>
+                <label>
+                  Email:
+                  <input type="email" name="email" />
+                </label>
+                <input
+                  type="submit"
+                  value="Send Verification Code"
+                  onClick={() => setLoginState("promptCode")}
+                />
+              </form>
+            </div>
+          )}
+
+          {loginState === "promptCode" && (
+            <div>
+              <h1>Enter Verification Code</h1>
+              <form>
+                <label>
+                  Code:
+                  <input type="text" name="code" />
+                </label>
+                <input
+                  type="submit"
+                  value="Login"
+                  onClick={async (event) => {
+                    event.preventDefault();
+                    await logIn(event.target.value);
+                    setLoginState("loggedIn");
+                  }}
+                />
+              </form>
+            </div>
+          )}
+
+          {loginState === "loggedIn" && !user.images.length && (
+            <div>
+              <h1>Create your Vana Portrait</h1>
+              <p>It seems we don't have a model for you yet.</p>
+              <input
+                type="submit"
+                value="Create Portrait on Vana"
+                onClick={() =>
+                  window
+                    .open("https://portrait.vana.com/create", "_blank")
+                    .focus()
+                }
+              />
+            </div>
+          )}
+
+          {loginState === "loggedIn" && user.images.length && (
+            <div>
+              <Generator />
+              {user.images.map((image, i) => (
+                <img src={image} key={i} />
+              ))}
+            </div>
+          )}
         </div>
 
         <div className={styles.grid}>
@@ -51,9 +167,7 @@ export default function Home() {
             <h2 className={inter.className}>
               GitHub <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              View this app on GitHub.
-            </p>
+            <p className={inter.className}>View this app on GitHub.</p>
           </a>
 
           <a
@@ -66,13 +180,12 @@ export default function Home() {
               Next.js Docs <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Find information about the Next.js framework that this template is built on.
+              Find information about the Next.js framework that this template is
+              built on.
             </p>
           </a>
-
-
         </div>
       </main>
     </>
-  )
+  );
 }
