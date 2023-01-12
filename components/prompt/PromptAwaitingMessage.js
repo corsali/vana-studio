b46b7ca567f2b83b2a18a104fc92c21549414d65
Callum Flack <callum@vana.org>
2023-01-12 15:18:25 +1100

    Update components/prompt/PromptAwaitingMessage.js
    Co-authored-by: dmitryvlass <121915729+dmitryvlass@users.noreply.github.com>

--- components/prompt/PromptAwaitingMessage.js
+++ components/prompt/PromptAwaitingMessage.js
@@ -4,7 +4,7 @@ const PromptAwaitingMessage = () => {
   return (
     <div className="space-y-4">
       <h2 className="">Takes about a minute</h2>
-      <p className="">While you're waiting, here's some other ideas:</p>
+      <p>While you're waiting, here's some other ideas:</p>
       <PromptIdeas className="pl-3" />
       <p>
         Got a problem? <a href="mailto:neil@vana.com" style={{ textDecoration: "underline"}}>Ask Neil.</a>{" "}
