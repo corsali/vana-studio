41f32160fe4c9bbe8ac56418dcc12f3532f3b3a0
Callum Flack <callum@vana.org>
2023-01-12 15:21:56 +1100

    Update components/prompt/IdeasMessage.js
    Co-authored-by: dmitryvlass <121915729+dmitryvlass@users.noreply.github.com>

--- components/prompt/IdeasMessage.js
+++ components/prompt/IdeasMessage.js
@@ -3,7 +3,7 @@ import { PromptIdeas } from "components";
 const IdeasMessage = () => {
   return (
     <div className="space-y-4">
-      <h2 className="">Stuck for ideas?</h2>
+      <h2>Stuck for ideas?</h2>
       <p className="">Sometimes, all it takes is attention.</p>
       <PromptIdeas className="pl-3" />
       <p>
