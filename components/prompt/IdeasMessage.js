aaf5099d5b0479e3be443e7ec38f480156f4693b
Callum Flack <callum@vana.org>
2023-01-13 10:36:10 +1100

    remove Neil's email link from the Ideas message

--- components/prompt/IdeasMessage.js
+++ components/prompt/IdeasMessage.js
@@ -6,9 +6,6 @@ const IdeasMessage = () => {
       <h2>Stuck for ideas?</h2>
       <p>Sometimes, all it takes is attention.</p>
       <PromptIdeas className="pl-3" />
-      <p>
-        Still stuck? <a href="mailto:neil@vana.com" style={{ textDecoration: "underline"}}>Ask Neil.</a>
-      </p>
     </div>
   );
 };
