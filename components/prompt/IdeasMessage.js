29a1beee4c828f683b97956fcadb89b98dce0b95
Callum Flack <callum@vana.org>
2023-01-12 15:22:08 +1100

    Update components/prompt/IdeasMessage.js
    Co-authored-by: dmitryvlass <121915729+dmitryvlass@users.noreply.github.com>

--- components/prompt/IdeasMessage.js
+++ components/prompt/IdeasMessage.js
@@ -4,7 +4,7 @@ const IdeasMessage = () => {
   return (
     <div className="space-y-4">
       <h2>Stuck for ideas?</h2>
-      <p className="">Sometimes, all it takes is attention.</p>
+      <p>Sometimes, all it takes is attention.</p>
       <PromptIdeas className="pl-3" />
       <p>
         Still stuck? <a href="mailto:neil@vana.com" style={{ textDecoration: "underline"}}>Ask Neil.</a>
