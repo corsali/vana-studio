563b5e67c744be1f51a838ac24ca58542ce25a04
Callum Flack <callum@vana.org>
2023-01-17 10:31:33 +1000

    Remove unused SVG in components/prompt/Marker.js
    Co-authored-by: dmitryvlass <121915729+dmitryvlass@users.noreply.github.com>

--- components/prompt/Marker.js
+++ components/prompt/Marker.js
@@ -3,25 +3,6 @@ import styles from "./Prompt.module.css";
 const Marker = ({ showArrow, children }) => {
   return (
     <span className={styles.marker}>
-      {/* {showArrow && (
-        <span className={styles.markerArrow}>
-          <svg
-            width="10"
-            height="22.5"
-            viewBox="0 0 12 27"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M11.7736 17L6.00006 27L0.226553 17L5.00006 17L5.00006 -2.96079e-07L7.00006 -2.08657e-07L7.00006 17L11.7736 17Z"
-              fill="currentColor"
-            />
-          </svg>
-        </span>
-      )} */}
-
       {children}
     </span>
   );
