e67debd8c8733dfc6d460dbb46e9115db5612fdc
dmitryvlass <121915729+dmitryvlass@users.noreply.github.com>
2023-02-02 15:17:36 +1100

    Include Link to Buy more Credits (#31)

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 const config = {
+  APP_BASE_URL: "https://studio.vana.com",
+  STRIPE_PRODUCT_100_CREDITS: "price_1MFSirJlfQgmiz8ARWF0hDvZ",
+  STRIPE_PRODUCT_250_CREDITS: "price_1MFSiDJlfQgmiz8AkVLSaYvw",
+  REMIX_SINGLE_CREDIT_PRICE: 0.1,
   VANA_API_URL: "https://api.vana.com/api/v0",
   VANA_GITHUB_URL: "https://github.com/corsali/vana-portrait-demo",
   VANA_PORTRAIT_URL: "https://portrait.vana.com",
