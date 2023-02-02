4f51c57de52328161999a8d252bb9374baa06b77
Callum Flack <callum@vana.org>
2023-02-03 09:07:40 +1000

    refactor Footer layout

--- components/nav/Footer.js
+++ components/nav/Footer.js
@@ -2,18 +2,23 @@ import config from "config";
 
 const Footer = () => {
   return (
-    <footer className="container mx-auto text-center pb-lg text-gray text-3">
-      <div className="mb-3 mx-auto">
-        <hr style={{ border: "initial", borderTop: "1px solid #ddd"}} />
+    <footer className="container mx-auto text-center pb-lg text-gray text-3 space-y-5">
+      <div className="mx-auto">
+        <hr style={{ border: "initial", borderTop: "1px solid #ddd" }} />
       </div>
-      <p>If you have any issues, please reach out to: <a href="mailto:support@vana.com">support@vana.com</a></p><br/>
-      <a href={config.vanaTermsURL} className="link" target="_blank">
-        Terms of Service
-      </a>
-      &nbsp;&nbsp;&nbsp;
-      <a href={config.vanaPrivacyURL} className="link" target="_blank">
-        Privacy Policy
-      </a>
+      <p className="pt-3">
+        If you have any issues, please reach out to:{" "}
+        <a href="mailto:support@vana.com">support@vana.com</a>
+      </p>
+      <p className="flex justify-center gap-4 h-full">
+        <a href={config.vanaTermsURL} className="link" target="_blank">
+          Terms of Service
+        </a>
+        <div className="divider" style={{ height: "18px", flex: "initial" }}></div>
+        <a href={config.vanaPrivacyURL} className="link" target="_blank">
+          Privacy Policy
+        </a>
+      </p>
     </footer>
   );
 };
