645b1fd2223d6a51bfb27e9eb23b13f4ae27ed36
Callum Flack <callum@vana.org>
2023-02-02 10:46:53 +1000

    Update config.js
    Co-authored-by: dmitryvlass <121915729+dmitryvlass@users.noreply.github.com>

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   VANA_API_URL: "https://api.vana.com/api/v0",
   VANA_GITHUB_URL: "https://github.com/corsali/vana-portrait-demo",
   VANA_PORTRAIT_URL: "https://portrait.vana.com",
-  vanaSupportEmail: "support@vana.com",
+  VANA_SUPPORT_EMAIL: "support@vana.com",
 };
 
 module.exports = config;
