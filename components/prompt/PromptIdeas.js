83f16f6ae1a08f97a80c9e69d2cfd250df7a2a39
Zach Hay <zach@vana.org>
2023-02-06 16:30:01 -0800

    remove names from prompt ideas

--- components/prompt/PromptIdeas.js
+++ components/prompt/PromptIdeas.js
@@ -1,7 +1,7 @@
 export const ideas = [
-  "Me portrait, fine details. bubblegum pink, realistic shaded lighting poster, character concept art trending on pixiv by ilya kuvshinov, katsuhiro otomo, magali villeneuve, artgerm, jeremy lipkin, rob rey",
-  "Me closeup portrait, mystic landscape, fantasy, rose gold, maze deep dimensional world, epic background, symmetrical, Greg Rutkowski, Charlie Bowater, Beeple, Unreal 5, hyperrealistic, dynamic lighting, fantasy art",
-  "Me angelic closeup portrait, halo, celestial world by greg rutkowski, intricate, epic fantasy, elegant, regal, by stanley artgerm lau, greg rutkowski, norman rockwell, asymmetric, dynamic composition, cream and purple",
+  "Me portrait, fine details. bubblegum pink, realistic shaded lighting poster, character concept art trending on pixiv, artgerm",
+  "Me closeup portrait, mystic landscape, fantasy, rose gold, maze deep dimensional world, epic background, symmetrical, Beeple, Unreal 5, hyperrealistic, dynamic lighting, fantasy art",
+  "Me angelic closeup portrait, halo, celestial world, intricate, epic fantasy, elegant, regal, asymmetric, dynamic composition, cream and purple",
 ];
 
 const PromptIdeas = ({ className }) => {
