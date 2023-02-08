bbb028e8a9552901d75da4db771ab2d483d3e897
Joseph Csoti <joseph@vana.org>
2023-02-08 13:16:18 -0500

    Update PromptIdeas.js

--- components/prompt/PromptIdeas.js
+++ components/prompt/PromptIdeas.js
@@ -1,6 +1,6 @@
 export const ideas = [
-  "Me portrait, fine details. bubblegum pink, realistic shaded lighting poster, character concept art trending on pixiv, artgerm",
-  "Me closeup portrait, mystic landscape, fantasy, rose gold, maze deep dimensional world, epic background, symmetrical, Beeple, Unreal 5, hyperrealistic, dynamic lighting, fantasy art",
+  "Me digital art painting, bubblegum pink, realistic shaded lighting poster, character concept art trending on pixiv, pop art, kitsch, imaginative",
+  "Me fantasy painting portrait, vibrant magical landscape, rose gold, starry dimensional world, epic background, dynamic lighting, fantasy art, stylized, rich colors",
   "Me angelic closeup portrait, halo, celestial world, intricate, epic fantasy, elegant, regal, asymmetric, dynamic composition, cream and purple",
 ];
 
