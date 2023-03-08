3b1ba134a8d45febbf7a8563f0f1de7beac16003
Joseph Csoti <joseph@vana.com>
2023-03-08 16:20:38 -0500

    Update typeform links (#42)
    usevana usevana usevana usevana usevana usevana

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
   VANA_PORTRAIT_URL: "https://portrait.vana.com",
   VANA_SUPPORT_EMAIL: "support@vana.com",
   VANA_BUILDER_EMAIL: "build@vana.com",
-  TYPEFORM_BUILDER_URL: "https://4eyuszgyknu.typeform.com/hey-builders",
+  TYPEFORM_BUILDER_URL: "https://usevana.typeform.com/hey-builders",
   vanaTermsURL: "https://www.vana.com/terms-of-service",
   vanaPrivacyURL: "https://www.vana.com/privacy-policy",
 };
